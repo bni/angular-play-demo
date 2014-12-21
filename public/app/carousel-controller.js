72c49a6daf2d06c79df35ec17dfe6adcdbdc2771
bni <bnilsson@me.com>
2014-12-21 16:06:03 +0100

    Add carousel

---
+++ public/app/carousel-controller.js
@@ -0,0 +1,11 @@
+phoneControllers.controller('CarouselCtrl', function ($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+        var slides = $scope.slides = [];
+
+        for (var i = 0; i < phone.images.length; i++) {
+            slides.push({
+                image: phone.images[i]
+            });
+        }
+    });
+});
