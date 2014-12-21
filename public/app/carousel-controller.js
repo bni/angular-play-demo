d55d9a8ac05e58236c4109294c8d6ea9a97ac3a3
bni <bnilsson@me.com>
2014-12-21 19:20:12 +0100

    Reorg angular includes

--- public/app/carousel-controller.js
+++ public/app/carousel-controller.js
@@ -1,3 +1,5 @@
+'use strict';
+
 phoneControllers.controller('CarouselCtrl', function ($scope, $routeParams, Phone) {
     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
         var slides = $scope.slides = [];
