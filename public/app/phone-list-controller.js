5cfd7d471af0ae191fa6ab74fba4dfa5b19dbd74
bni <bnilsson@me.com>
2014-12-23 14:25:32 +0100

    Select row

--- public/app/phone-list-controller.js
+++ public/app/phone-list-controller.js
@@ -2,4 +2,9 @@
 
 phoneControllers.controller('PhoneListCtrl', function ($scope, Phone) {
     $scope.phones = Phone.query();
+
+    $scope.idSelected = null;
+    $scope.setSelected = function (idSelected) {
+        $scope.idSelected = idSelected;
+    };
 });
