81d29ee13927ed0cfc12668701a22c33154e59d9
bni <bnilsson@me.com>
2015-01-01 16:37:02 +0100

    Add modal confirm dialog

---
+++ public/app/controllers/confirm-controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+phoneControllers.controller('ConfirmDialogCtrl', function ($scope, $modalInstance, dialogTitle, dialogMessage, dialogConfirm, dialogCancel) {
+    $scope.dialogTitle = dialogTitle;
+    $scope.dialogMessage = dialogMessage;
+    $scope.dialogConfirm = dialogConfirm;
+    $scope.dialogCancel = dialogCancel;
+
+    $scope.confirm = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+});
--- public/app/controllers/phone-list-controller.js
+++ public/app/controllers/phone-list-controller.js
@@ -1,10 +1,35 @@
 'use strict';
 
-phoneControllers.controller('PhoneListCtrl', function ($scope, Phone) {
+phoneControllers.controller('PhoneListCtrl', function ($scope, Phone, $modal) {
     $scope.phones = Phone.query();
 
-    $scope.idSelected = null;
-    $scope.setSelected = function (idSelected) {
-        $scope.idSelected = idSelected;
+    $scope.selectedPhone = null;
+    $scope.setSelectedPhone = function (selectedPhone) {
+        $scope.selectedPhone = selectedPhone;
+    };
+
+    $scope.deletePhone = function() {
+        var confirmDialog = $modal.open({
+            templateUrl: 'templates/confirm-dialog.html',
+            controller: 'ConfirmDialogCtrl',
+            resolve: {
+                dialogTitle: function() {
+                    return 'Please confirm';
+                },
+                dialogMessage: function() {
+                    return 'Are you sure you want to delete ' + $scope.selectedPhone.name + '?';
+                },
+                dialogConfirm: function() {
+                    return 'Delete'
+                },
+                dialogCancel: function() {
+                    return 'Cancel'
+                }
+            }
+        });
+
+        confirmDialog.result.then(function () {
+            console.log('Performing delete of: ' + $scope.selectedPhone.id);
+        });
     };
 });
